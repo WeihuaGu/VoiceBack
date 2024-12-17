def2d05b0f8dc0092b12d79730f33922389c9d6f
notepad <haha@hah.com>
2024-12-17 21:28:54 +0800

    oo

--- vad.js
+++ vad.js
@@ -1,14 +1,37 @@
 import { Transform } from 'stream';
+import axios from 'axios';
+import FormData from 'form-data';
+
 
 class VadTransform extends Transform {
     constructor(rec_options) {
 	super();
 	this.rec_options = rec_options;
+	this.url_vadcheck = 'http://127.0.0.1:5000/check_audio'
     }
 
     _transform(chunk, encoding, next) {
-	this.push(chunk);
-        next();
+	    const formData = new FormData();
+	    formData.append('audio', Buffer.from(chunk), 'audio.wav');
+	    const me = this;
+        try {
+            const response = axios.post(this.url_vadcheck, formData, {
+                headers: {
+                    'Content-Type':'multipart/form-data'
+                }
+            });
+	    response.then((res)=>{
+                if (res.data === "The audio contains human voice") {
+                   me.push(chunk);
+                   next();
+                }
+	    })
+
+        } catch (error) {
+            console.error("Error while checking audio for voice:", error);
+            next(error);
+        }
+
     }
 }
 
