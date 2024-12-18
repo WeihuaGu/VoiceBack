cfd67927d34bb5a263832f20a60f2a236e3c4cca
dingdang <dingdang@haha.com>
2024-12-18 11:12:01 +0800

    让vad.js请求python服务判断是不是vad

--- vad.js
+++ vad.js
@@ -7,30 +7,68 @@ class VadTransform extends Transform {
     constructor(rec_options) {
 	super();
 	this.rec_options = rec_options;
-	this.url_vadcheck = 'http://127.0.0.1:5000/check_audio'
+        this.sampleWidth = int(rec_options.bits/8);
+        this.sampleRate =  rec_options.sampleRate;
+	this.url_vadcheck = 'http://127.0.0.1:5000/check_audio';
     }
+    splitFrames(chunk,frameDuration) {
+        const frameSizeInBytes = Math.floor(this.sampleRate * (frameDuration / 1000) * 2 * 1);
+        const numFrames = Math.floor(chunk.length / frameSizeInBytes);
+        const frames = [];
 
-    _transform(chunk, encoding, next) {
-	    const formData = new FormData();
-	    formData.append('audio', Buffer.from(chunk), 'audio.wav');
-	    const me = this;
-        try {
-            const response = axios.post(this.url_vadcheck, formData, {
+        for (let i = 0; i < numFrames; i++) {
+            const start = i * frameSizeInBytes;
+            const end = start + frameSizeInBytes;
+            const frameChunk = chunk.slice(start, end);
+            frames.push(frameChunk);
+        }
+        return frames;
+    }
+    sendFrameToPythonService(frameChunk) {
+        // 根据采样宽度进行相应处理，这里针对16位音频提取低字节发送给Python服务
+        let processedFrameChunk;
+        if (this.sampleWidth === 2) {
+            processedFrameChunk = frameChunk.filter((_, index) => index % 2!== 0);
+        } else {
+            processedFrameChunk = frameChunk;
+        }
+        const formData = new FormData();
+        formData.append('audio', Buffer.from(processedFrameChunk));
+
+        const posturl = this.url_vadcheck;
+	return new Promise((resolve, reject) => {
+            axios.post(posturl, formData, {
                 headers: {
                     'Content-Type':'multipart/form-data'
                 }
-            });
-	    response.then((res)=>{
-                if (res.data === "The audio contains human voice") {
-                   me.push(chunk);
-                   next();
-                }
-	    })
+            }).then((response)=>{
+		    resolve(response.data === "The audio contains human voice");
+	    }).catch((error)=>{
+                    console.error("Error while checking audio for voice:", error);
+                    reject(false);
 
-        } catch (error) {
-            console.error("Error while checking audio for voice:", error);
-            next(error);
-        }
+	    });
+
+	});
+
+    }
+
+    _transform(chunk, encoding, next) {
+       const self = this;
+       const frames = this.splitFrames(chunk,30);
+       const voiceCheckPromises = frames.map(frameChunk => this.sendFrameToPythonService(frameChunk));
+       Promise.all(voiceCheckPromises)
+	    .then(()=>{
+		        results.forEach((isVoice, index) => {
+                          if (isVoice) 
+                            self.push(frames[index]);
+			});
+		        next();
+            }).catch((err)=>{
+               console.error("Error while checking audio for voice in _transform:", error);
+               next(error);
+
+	    });
 
     }
 }
