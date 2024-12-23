f002a30fdb7c4720dfe51f831dbe7ddab0000605
dingd <haha@mail.com>
2024-12-23 16:09:30 +0800

    hahapy

--- server.py
+++ server.py
@@ -1,23 +1,26 @@
 import io
+import json
 from flask import Flask, request
 from vad import is_speechdata
 
 app = Flask(__name__)
 
-@app.route('/check_audio', methods=['POST'])
+@app.route('/is_human_audio', methods=['POST'])
 def check_audio():
     if 'audio' not in request.files:
         return "No audio file provided", 400
 
     audio_file = request.files['audio']
     audio_data = audio_file.read()
-
-    # 将接收到的音频数据转换为可被webrtcvad处理的格式（这里假设是WAV格式，可按需调整）
-    audio_io = io.BytesIO(audio_data)
-    is_voice = is_speechdata(audio_io)
+    is_voice = is_speechdata(audio_data);
+    res_json = {
+        "human_voice": is_voice,
+    }
     if is_voice:
-        return "The audio contains human voice", 200
-    return "The audio does not contain human voice", 200
+        print('The audio contains human voice');
+    else:
+        print('The audio does not contains human voice');
+    return json.dumps(res_json), 200, {'Content-Type': 'application/json'}
 
 
 if __name__ == '__main__':
