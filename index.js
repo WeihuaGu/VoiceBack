eccf3f17ef8d54c99ad083ebccacc2e0d94c3fa6
notepad <haha@hah.com>
2024-12-30 17:49:44 +0800

    ooo

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { rec_options ,getRecorder } from './recorder.js'
 import { VadTransform } from './vad.js'
 import { AudioSound } from './sound.js'
+import { AudioCompressor } from './compressor.js'
 const vadtrans = new VadTransform(rec_options);
+const compress = new AudioCompressor();
 const sound = new AudioSound(rec_options);
 //getRecorder().pipe(sound);
-getRecorder().pipe(vadtrans).pipe(sound);
+//getRecorder().pipe(vadtrans).pipe(sound);
+getRecorder().pipe(compress).pipe(sound);
